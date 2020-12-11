b4e7b595cb41693c38dced62bb8893de17403b81
Rajat Kulkarni <rajatkulkarni95@gmail.com>
2020-12-11 21:37:49 +0530

    Risk Algorithm added, make_response to send json out

--- project/controller/controller_risk.py
+++ project/controller/controller_risk.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, request, jsonify
+from flask import Blueprint, request, jsonify, make_response
 from flask_cors import CORS
+from project.static.risk_algorithm import calculate_risk_profile
 
 # Blueprint Config
 risk_blueprint = Blueprint("risk_blueprint", __name__)
@@ -9,4 +10,8 @@ CORS(risk_blueprint)
 
 @risk_blueprint.route('/risks', methods=['POST'])
 def risk_assessment():
-    pass
+    input_data = request.get_json()
+    risk_profile = calculate_risk_profile(input_data)
+
+    res = make_response(jsonify(risk_profile), 200)
+    return res
---
+++ project/static/risk_algorithm.py
@@ -0,0 +1,68 @@
+
+
+from datetime import datetime
+
+
+def calculate_risk_profile(data):
+    age, dependents, house, income, marital_status, risk_questions, vehicle = data.values()
+    risk_profile = {
+        "auto": 0,
+        "disability": 0,
+        "home": 0,
+        "life": 0,
+    }
+    base_score = sum(risk_questions)
+    try:
+        risk_profile = {key: base_score for key in risk_profile.keys()}
+
+        # Age related Risks
+        if age > 60:
+            risk_profile['disability'] = 'ineligible'
+            risk_profile['life'] = 'ineligible'
+
+        if age < 30:
+            risk_profile = {key: (value-2 if type(value) == int else value) for key,
+                            value in risk_profile.items()}
+        elif 30 < age < 40:
+            risk_profile = {key: (value-1 if type(value) == int else value) for key,
+                            value in risk_profile.items()}
+
+        # Mortgage Related Risks
+        if income > 200000:
+            risk_profile = {key: (value-1 if type(value) == int else value) for key,
+                            value in risk_profile.items()}
+
+        if house['ownership_status'] == 'mortgaged':
+            risk_profile['home'] += 1
+            risk_profile['disability'] += 1
+        if dependents > 0:
+            risk_profile['life'] += 1
+            risk_profile['disability'] += 1
+        if marital_status == 'married':
+            risk_profile['life'] += 1
+            risk_profile['disability'] -= 1
+
+        if vehicle['year'] > datetime.now().year - 5:
+            risk_profile['auto'] += 1
+
+        for key, value in risk_profile.items():
+            if value <= 0:
+                risk_profile[key] = 'economic'
+            elif value == 1 or value == 2:
+                risk_profile[key] = 'regular'
+            elif value >= 3:
+                risk_profile[key] = 'responsible'
+
+        # Ineligibile Clauses
+        if income == 0:
+            risk_profile['disability'] = 'ineligible'
+
+        if vehicle == 0:
+            risk_profile['auto'] = 'ineligible'
+
+        if house == 0:
+            risk_profile['home'] = 'ineligible'
+
+    except Exception as err:
+        print(err)
+    return risk_profile
